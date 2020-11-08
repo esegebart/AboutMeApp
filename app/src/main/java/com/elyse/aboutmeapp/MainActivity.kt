07b87f891ada0af45cc386fa1b5c377ac7e52426
esegebart <36378900+esegebart@users.noreply.github.com>
2020-11-08 17:23:38 -0600

    Added data binding to name and nickname text.

--- app/src/main/java/com/elyse/aboutmeapp/MainActivity.kt
+++ app/src/main/java/com/elyse/aboutmeapp/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.elyse.aboutmeapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.elyse.aboutmeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+   private lateinit var binding: ActivityMainBinding
+
+   private val myName: MyName = MyName("Elyse Segebart")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //Set value of myName variable that is declared and used in the layout file
+        binding.myName = myName
+
+        // Set onclicklistener for button and set reference for click handler
+        //findViewById<Button>(R.id.done_button).setOnClickListener {
+        //    addNickname(it)
+        //}
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+
+        binding.apply {
+            binding.nicknameText.text = binding.nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            binding.nicknameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+        }
+
+        // Hide the keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
